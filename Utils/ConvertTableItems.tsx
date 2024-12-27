f4e0de9315aba1b5d671fc2ab188780514e39529
SerkanAfsar <serkan-afsar@hotmail.com>
2024-12-27 15:09:41 +0300

    Fix

--- Utils/ConvertTableItems.tsx
+++ Utils/ConvertTableItems.tsx
@@ -1,6 +1,5 @@
 import { BpOrderType } from "@/Types/BpOrder.Types";
 import { CarType } from "@/Types/Car.Types";
-import { _ } from "gridjs-react";
 
 export const returnCarItem = (responseData: any[]) => {
   return responseData.map((item: CarType) => [
